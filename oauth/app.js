069e078ff61650f6f347e15f8812c80d8f8ecd58
Ian Homer <ihomer@worth.systems>
2019-05-29 12:04:04 +0100

    ONS-38 : Apply linting

--- oauth/app.js
+++ oauth/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({silent: true})
+require('dotenv').config({ silent: true })
 const express = require('express')
 const simpleOauthModule = require('simple-oauth2')
 const randomString = require('randomstring')
@@ -18,68 +18,68 @@ const OAUTH_PROVIDER = 'github'
 const LOGIN_AUTH_TARGET = '_self'
 
 if (!OAUTH_CLIENT_ID) {
-    if (GAE_INSTANCE) {
-        console.error(`FATAL : OAUTH_CLIENT_ID is not set, was this app deployed with environment variables set?`)
-    } else {
-        console.error(`FATAL : OAUTH_CLIENT_ID is not set, do you have a local .env file in place?`)
-    }
-    process.exit()
+  if (GAE_INSTANCE) {
+    console.error(`FATAL : OAUTH_CLIENT_ID is not set, was this app deployed with environment variables set?`)
+  } else {
+    console.error(`FATAL : OAUTH_CLIENT_ID is not set, do you have a local .env file in place?`)
+  }
+  process.exit()
 }
 
 const app = express()
 const oauth2 = simpleOauthModule.create({
-    client: {
-        id: OAUTH_CLIENT_ID,
-        secret:OAUTH_CLIENT_SECRET
-    },
-    auth: {
-        tokenHost: 'https://github.com',
-        tokenPath: '/login/oauth/access_token',
-        authorizePath: '/login/oauth/authorize'
-    }
+  client: {
+    id: OAUTH_CLIENT_ID,
+    secret: OAUTH_CLIENT_SECRET
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize'
+  }
 })
 
 if (('').match(ORIGIN)) {
-    console.error(`FATAL : Insecure ORIGIN pattern : ${ORIGIN}`)
-    process.exit()
+  console.error(`FATAL : Insecure ORIGIN pattern : ${ORIGIN}`)
+  process.exit()
 }
 
 // Authorization uri definition
 const authorizationUri = oauth2.authorizationCode.authorizeURL({
-    redirect_uri: REDIRECT_URL,
-    scope: SCOPES,
-    state: randomString.generate(32)
+  redirect_uri: REDIRECT_URL,
+  scope: SCOPES,
+  state: randomString.generate(32)
 })
 
 // Initial page redirecting to Github
 app.get('/auth', (req, res) => {
-    res.redirect(authorizationUri)
+  res.redirect(authorizationUri)
 })
 
 // Callback service parsing the authorization token and asking for the access token
 app.get('/callback', (req, res) => {
-    const code = req.query.code
-    const options = {
-        code: code
+  const code = req.query.code
+  const options = {
+    code: code
+  }
+
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    let mess, content
+
+    if (error) {
+      console.error('Access Token Error', error.message)
+      mess = 'error'
+      content = JSON.stringify(error)
+    } else {
+      const token = oauth2.accessToken.create(result)
+      mess = 'success'
+      content = {
+        token: token.token.access_token,
+        provider: OAUTH_PROVIDER
+      }
     }
 
-    oauth2.authorizationCode.getToken(options, (error, result) => {
-        let mess, content
-
-        if (error) {
-            console.error('Access Token Error', error.message)
-            mess = 'error'
-            content = JSON.stringify(error)
-        } else {
-            const token = oauth2.accessToken.create(result)
-            mess = 'success'
-            content = {
-                token: token.token.access_token,
-                provider: OAUTH_PROVIDER
-            }
-        }
-
-        const script = `
+    const script = `
     <script>
     (function() {
       function recieveMessage(e) {
@@ -98,16 +98,16 @@ app.get('/callback', (req, res) => {
       window.opener.postMessage("authorizing:${OAUTH_PROVIDER}", "*")
     })()
     </script>`
-        return res.send(script)
-    })
+    return res.send(script)
+  })
 })
 
 app.get('/success', (req, res) => {
-    res.send('')
+  res.send('')
 })
 
 app.get('/', (req, res) => {
-    res.send(`<br>
+  res.send(`<br>
     <a href="/auth" target="${LOGIN_AUTH_TARGET}">
       Log in with ${OAUTH_PROVIDER.toUpperCase()}
     </a>`)
@@ -115,5 +115,5 @@ app.get('/', (req, res) => {
 
 // Start the server;
 app.listen(PORT, () => {
-    console.log(`oauth service running on port ${PORT}`)
+  console.log(`oauth service running on port ${PORT}`)
 })
