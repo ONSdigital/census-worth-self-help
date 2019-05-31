abc77e70f1ca2b1fb5db2e084262530b5ee4239f
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-05-31 16:28:29 +0100

    more gatsby nodes refactor

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -17,6 +17,7 @@ function fetchArticlesAndRepos(data) {
         {title : node.frontmatter.title,
          parent_title : node.frontmatter.repo,
          link: filename,
+         breadcrumbs: [],
          type: "article"})
     } else if( directory === 'repos' && node.frontmatter.repo ) {
       repos.push(
@@ -24,6 +25,7 @@ function fetchArticlesAndRepos(data) {
          parent_title : node.frontmatter.repo,
          resolved : false,
          children: [],
+         breadcrumbs: [],
          link: filename,
          type: "repo"})
     }
@@ -31,7 +33,7 @@ function fetchArticlesAndRepos(data) {
   return {articles: articles, repos: repos}
 }
 
-function connectRepos(repos) {
+function connectReposTogether(repos) {
   // Repos is a list of the directories with only the root element resolved.
   // We loop through the list of repos, each time we will connect one layer further until we find no more layers.
   // If there are any loops that do not get connected to the root, they will not be included.
@@ -48,19 +50,12 @@ function connectRepos(repos) {
           repo.resolved = true
           repo.parent = parent
           parent.children.push(repo)
+          repo.breadcrumbs = parent.breadcrumbs.slice() // shallow copy
+          repo.breadcrumbs.push({title: parent.title, link: parent.link})
         }
       }
     })
   }
-}
-
-function breadcrumbRecursion(repo, breadcrumbs) {
-  if(repo.parent)
-  {
-    breadcrumbs = breadcrumbRecursion(repo.parent, breadcrumbs)
-  }
-  breadcrumbs.push({title: repo.title, link: repo.link})
-  return breadcrumbs
 }
 
 function combineArticlesAndRepos(articles, repos) {
@@ -70,18 +65,21 @@ function combineArticlesAndRepos(articles, repos) {
     {
       article.parent = parent
       parent.children.push(article)
+      article.breadcrumbs = article.breadcrumbs.slice() // shallow copy
+      article.breadcrumbs.push({title: parent.title, link: parent.link})
     }
   })
 }
 
 function createMenuPage(createPage, rootRepo) {
+  // The tree we need for the menu starts at the root and recurses down.
   let menuRecursion = function(repo, menutree) {
     let menutreeElement = {title: repo.title, link: repo.link}
     menutreeElement.children = repo.children.map( (child) => menuRecursion(child, menutreeElement))
     return menutreeElement
   }
-
   let menutree = menuRecursion(rootRepo, [])
+
   createPage({
       path: "menu",
       component: path.resolve(`./src/templates/menu-template.js`),
@@ -93,20 +91,17 @@ function createMenuPage(createPage, rootRepo) {
 function createArticlePages(createPage, articles) {
   articles.forEach(( article ) => {
     let peers = []
-    let breadcrumbs = []
     if(article.parent)
     {
       peers = article.parent.children.map( (child) => { return {title: child.title, link: child.link} })
-      breadcrumbs = breadcrumbRecursion(article.parent, [])
     }
-
     createPage({
         path: article.link,
         component: path.resolve(`./src/templates/standard-article.js`),
         context: {
           title : article.title, 
           peers : peers,
-          breadcrumbs : breadcrumbs}
+          breadcrumbs : article.breadcrumbs}
     })
   })
 }
@@ -117,11 +112,9 @@ function createRepoPages(createPage, repos)
     // Figure out ancestors, peers, and children before creating repo pages.
     let children = repo.children.map( (child) => { return { title: child.title, link: child.link, type: child.type} })
     let peers = []
-    let breadcrumbs = []
     if(repo.parent)
     {
       peers = repo.parent.children.map( (child) => { return {title: child.title, link: child.link} })
-      breadcrumbs = breadcrumbRecursion(repo.parent, [])
     }
 
     createPage({
@@ -131,7 +124,7 @@ function createRepoPages(createPage, repos)
         title : repo.title,
         children : children,
         peers : peers,
-        breadcrumbs : breadcrumbs
+        breadcrumbs : repo.breadcrumbs
       },
     })
   })
@@ -157,18 +150,19 @@ exports.createPages = ({ graphql, actions }) => {
   `
 ).then(result => {
 
-    {repos, articles} = fetchArticlesAndRepos(result.data.allMarkdownRemark.edges)
+    let {repos, articles} = fetchArticlesAndRepos(result.data.allMarkdownRemark.edges)
 
-    let rootRepo = {title : 'Root', resolved : true, parent: null, children: [], link: "explore"}
+    let rootRepo = {title : 'Root', resolved : true, parent: null, children: [], breadcrumbs: [], link: "explore"}
     repos.push(rootRepo)
 
-    connectRepos(repos)
+    connectReposTogether(repos)
 
     createMenuPage(createPage, rootRepo)
 
     combineArticlesAndRepos(articles, repos)
 
     createArticlePages(createPage, articles)
+
     createRepoPages(createPage, repos)
   })
 }
