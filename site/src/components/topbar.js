7101bcb3c62bff99268cfc43ccb372549a75c79b
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-06-07 16:48:14 +0100

    workflow to an extent

--- site/src/components/topbar.js
+++ site/src/components/topbar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { css } from "@emotion/core"
-import { gradients, spacing } from "../utils/styles"
+import { gradients, spacing, colors } from "../utils/styles"
 import TopbarLink from "./topbarlink"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -8,6 +8,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons"
 
 export default ( {searchObject=null} ) => {
   let searchOpen = searchObject && searchObject.open
+  console.log(searchObject)
   return (
     <header
       css={css`
@@ -15,7 +16,34 @@ export default ( {searchObject=null} ) => {
         ${gradients.navy_shine};
       `}
     >
-      { searchOpen && <input type="text" value={searchObject.query} onChange={searchObject.updateFunction} /> }
+      { searchOpen && 
+        <div css={css`
+          display: flex;
+        `}>
+          <div css={css`
+                padding: 10px 15px 15px 15px;
+                flex-grow: 1;
+                display: flex;
+              `}>
+            <div css={css`
+              ${spacing.vert_aligned_flex_text}
+              position: relative;
+              left:5px;
+            `}><FontAwesomeIcon css={css`
+              position: absolute;
+              color: #6e6e6e;
+            `} icon={faSearch} /></div>
+            <input className="Notification-heading-Style" css={css`
+                  height: 36px;
+                  border-radius: 10px;
+                  background-color: ${colors.white_two};
+                  flex-grow: 1;
+                  padding-left:25px;
+              `}
+              type="text" value={searchObject.query} onChange={searchObject.updateFunction} onBlur={searchObject.finishedFunction} autoFocus />
+          </div>
+          <TopbarLink title="Cancel" link="/" background={false} />
+        </div> }
       { !searchOpen && 
         <div css={css`
           display: flex;
@@ -30,8 +58,9 @@ export default ( {searchObject=null} ) => {
           >
             Census Field Assistant
           </div>
-          <TopbarLink title={<FontAwesomeIcon icon={faSearch} />} link="search" />
-          <TopbarLink title="Menu" link="menu" />
+          <TopbarLink title={<FontAwesomeIcon icon={faSearch} />} link="/search" 
+            clickFunction={searchObject ? searchObject.startFunction : null} />
+          <TopbarLink title="Menu" link="/menu" />
         </div>
       }
     </header>
--- site/src/pages/search.js
+++ site/src/pages/search.js
@@ -3,6 +3,9 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Index } from "elasticlunr"
 import TabList from "../components/tablist"
+import PageTitle from "../components/pagetitle"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
 
 export default class Search extends React.Component {
   constructor(props) {
@@ -10,12 +13,27 @@ export default class Search extends React.Component {
     this.state = {
       query: ``,
       results: [],
+      finished: false
     }
     this.data = props.data
     this.updateSearchResults = this.updateSearchResults.bind(this)
+    this.finishedSearching = this.finishedSearching.bind(this)
+    this.startSearching = this.startSearching.bind(this)
   }
 
   componentWillMount() {}
+
+  finishedSearching() {
+    this.setState({
+      finished : true})
+  }
+
+  startSearching(e) {
+    e.preventDefault();
+    console.log("clicked")
+    this.setState({
+      finished : false})
+  }
 
   updateSearchResults(evt) {
     const query = evt.target.value
@@ -32,22 +50,31 @@ export default class Search extends React.Component {
 
   render() {
     let searchObject = {
-      open: true,
+      open: !this.state.finished,
       updateFunction: this.updateSearchResults,
-      query: this.state.query
+      query: this.state.query,
+      finishedFunction: this.finishedSearching,
+      startFunction: this.startSearching
     }
     let edges = this.state.results.map( result => {
       return this.data.allMarkdownRemark.edges.find( edge => edge.node.frontmatter.title === result.title )
     })
+
+    let searching = this.state.query.length >= 4 // todo: figure out how this is actually calculated
+
     return (
-      <Layout title="Search" searchObject={searchObject}>
+      <Layout title="Search" searchObject={searchObject} explore_more_link={this.state.finished}>
+        { searching && 
+          <div>
+            { edges.length > 0 && <PageTitle><FontAwesomeIcon icon={faSearch} /> {edges.length} results for "{this.state.query}"</PageTitle> }
+            { edges.length===0 && <PageTitle><FontAwesomeIcon icon={faSearch} /> Sorry no results for "{this.state.query}"</PageTitle> }
+          </div>
+        }
         <TabList elements={edges} />
       </Layout>
     )
   }
 }
-
-
 
 export const query = graphql`
   query {
