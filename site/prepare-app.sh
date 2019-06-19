001be1452faf49af305f1d41377d049e7b47fa46
Ian Homer <ihomer@worth.systems>
2019-06-19 16:41:32 +0100

    ONS-9 : Tweaks for app.yaml

--- site/prepare-app.sh
+++ site/prepare-app.sh
@@ -4,5 +4,5 @@ cp app.yaml ${APP_FILE}
 
 echo >> ${APP_FILE}
 echo "env_variables:" >> ${APP_FILE}
-sed "s/\(.*\)=\(.*\)/  \1: '\2'/" .env >> ${APP_FILE}
+sed "s/\([^=]*\)=\(.*\)/  \1: '\2'/" .env >> ${APP_FILE}
 
