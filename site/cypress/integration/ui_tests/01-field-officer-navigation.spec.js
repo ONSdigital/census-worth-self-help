7af29e788b5b4002d24462cff99640220a50a94a
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-06-19 11:06:06 +0100

    added e2e test

--- site/cypress/integration/ui_tests/01-field-officer-navigation.spec.js
+++ site/cypress/integration/ui_tests/01-field-officer-navigation.spec.js
@@ -6,6 +6,8 @@ const globalTestData = require('../../fixtures/globalTestData');
 // fragments
 const menu = require('../../fixtures/fragments/menu');
 const search = require('../../fixtures/fragments/search');
+const article = require('../../fixtures/fragments/article');
+const pagination = require('../../fixtures/fragments/pagination');
 
 // pages
 const homepage = require('../../fixtures/pages/homepagePage');
@@ -25,7 +27,7 @@ describe("The user flow", function() {
 
     it('Reading an article', function() {
         cy.get(homepage.articleCard).first().click();
-        cy.url().should('include', firstArticlePath);
+        cy.get(article.content).should('be.visible');
     });
 
     it('Opening the menu then closing it', function() {
@@ -62,6 +64,44 @@ describe("The user flow", function() {
         cy.get(search.searchBarField).type(authorName);
         cy.get(search.searchResultTitle).contains(authorName);
         cy.get(homepage.articleCard).should('have.text', articleName);
-        cy.get(search.pagination1).should('have.text', pageNumber1);
+        cy.get(pagination.pagination1).should('have.text', pageNumber1);
     });
+
+    it('Most recent pagination', function () {
+        // go to recently updated page
+        cy.get(menu.menuButton).contains('Menu').click();
+        cy.get(menu.menuLink).contains('Recently updated').click();
+
+        // page 1 should be selected. Clicking it again should do nothing.
+        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
+        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination1).click();
+        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
+        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+
+        // clicking page 2 should change switch selected pagination element
+        cy.get(pagination.pagination2).click();
+        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('match', /underline/)
+        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('not.match', /underline/)
+
+        // clicking back takes us back to 1 being selected
+        cy.get(pagination.back).click();
+        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
+        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.back).should('not.be.visible');
+
+        // clicking next takes us forward to 2 being selected
+        cy.get(pagination.next).click();
+        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('match', /underline/)
+        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('not.match', /underline/)
+
+        // after clicking next a couple more times, clicking first still takes us back to 1 being selected.
+        cy.get(pagination.next).click();
+        cy.get(pagination.next).click();
+        cy.get(pagination.first).click();
+        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
+        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.first).should('not.be.visible');
+     });
+
 });
