8beeb3e402f5fae7597f09b589c4a957a3854eca
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-06-19 09:27:01 +0100

    ONS-65, ONS-64, added pagination into myrecent and bookmarks

--- site/src/pages/bookmarks.js
+++ site/src/pages/bookmarks.js
@@ -9,34 +9,71 @@ import BlockStatus from "../components/blockstatus"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBookmark } from "@fortawesome/free-solid-svg-icons"
 
-export default ({ data }) => {
-  let bookmarkManager = new BookmarkManager()
-  let bookmarkTitles = bookmarkManager.getTopBookmarks()
-  let bookmarkEdges = bookmarkTitles.map(title =>
-    data.allMarkdownRemark.edges.find(
-      edge => edge.node.frontmatter.title === title
-    )
-  )
+import { PaginationObject } from "../utils/pagination"
+import PaginationBar from "../components/paginationbar"
 
-  return (
-    <Layout explore_more_link={true}>
-      <PageTitle>My Bookmarks</PageTitle>
-      <TextBlock>
-        Bookmarks are links to articles, which will appear here for you to
-        quickly find the article again. To remove a bookmark, tap on the
-        bookmark button in the article. Bookmarks are stored on your device, and
-        are linked to your account, so only you have access to them.
-      </TextBlock>
-      {bookmarkEdges.length > 0 && <TabList elements={bookmarkEdges} />}
-      {bookmarkEdges.length === 0 && (
-        <BlockStatus
-          icon={<FontAwesomeIcon icon={faBookmark} />}
-          title="Bookmarks will show here"
-          subtitle="Bookmarks are stored on your device"
-        />
-      )}
-    </Layout>
-  )
+export default class Bookmarks extends React.Component {
+  constructor(props) {
+    super(props)
+
+    let paginationObject = new PaginationObject()
+    this.state = {
+      paginationObject: paginationObject
+    }
+    this.data = props.data
+    this.updatePagination = this.updatePagination.bind(this)
+  }
+
+  updatePagination(pageTarget) {
+    this.state.paginationObject.goToPage(pageTarget)
+    // update state to get page to rerender
+    this.setState({
+      paginationObject: this.state.paginationObject
+    })
+  }
+
+  render() {
+    let bookmarkManager = new BookmarkManager()
+    let bookmarkTitles = bookmarkManager.getTopBookmarks()
+    let bookmarkEdges = bookmarkTitles.map(title =>
+      this.data.allMarkdownRemark.edges.find(
+        edge => edge.node.frontmatter.title === title
+      )
+    )
+    let paginatedBookmarkEdges = this.state.paginationObject.filterResults(
+      bookmarkEdges
+    )
+
+    return (
+      <Layout explore_more_link={true}>
+        <PageTitle>My Bookmarks</PageTitle>
+        <TextBlock>
+          Bookmarks are links to articles, which will appear here for you to
+          quickly find the article again. To remove a bookmark, tap on the
+          bookmark button in the article. Bookmarks are stored on your device,
+          and are linked to your account, so only you have access to them.
+        </TextBlock>
+        {bookmarkEdges.length > 0 && (
+          <TabList elements={paginatedBookmarkEdges} />
+        )}
+        {bookmarkEdges.length > 0 && (
+          <PaginationBar
+            total={bookmarkEdges.length}
+            paginationObject={this.state.paginationObject}
+            clickFunction={this.updatePagination}
+            onPageCount={paginatedBookmarkEdges.length}
+          />
+        )}
+        {bookmarkEdges.length === 0 && (
+          <BlockStatus
+            icon={<FontAwesomeIcon icon={faBookmark} />}
+            title="Bookmarks will show here"
+            subtitle="Bookmarks are stored on your device"
+          />
+        )}
+      </Layout>
+    )
+  }
 }
 
 export const query = graphql`
