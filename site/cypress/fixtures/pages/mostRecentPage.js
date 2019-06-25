878252483f8435013c67de59f42f35388d866eda
Fahad @Worth Internet Systems <50908458+fahmedWIS@users.noreply.github.com>
2019-06-25 14:46:40 +0100

    ONS-177 - Added tests to cover bookmarks. (#74)
    * ONS-177 - Added tests to cover bookmarks.
    
    * ONS-177 - Changed the test suite title.
    
    * ONS-177 - Cypress config change.
    
    * ONS-117 - Addressed review comment
    
    * ONS-117 - Added additional test cases and refactored some code.
    
    * ONS-177 Formatting.
    
    * ONS-177 Changed hard coded values to variables.

---
+++ site/cypress/fixtures/pages/mostRecentPage.js
@@ -0,0 +1,8 @@
+const mostRecentUrlPath = '/mostrecent';
+
+module.exports = {
+    articleCard: `[data-testid='articletab-article-card']`,
+    homepageTitle: `[data-test='mainLogo__title']`,
+    homepageLogo: `[data-test='ons-logo__header']`,
+    mostRecentUrlPath
+};
