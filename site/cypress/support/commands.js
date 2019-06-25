372a1bfd32782189fb7b8f6a6c404840e578c74d
fahmed <fahmed@worth.systems>
2019-06-25 14:00:54 +0100

    ONS-177 Formatting.

--- site/cypress/support/commands.js
+++ site/cypress/support/commands.js
@@ -1,8 +1,10 @@
-const bookmarks = require('../fixtures/fragments/bookmarks');
+const bookmarks = require("../fixtures/fragments/bookmarks")
 
-Cypress.Commands.add('bookmarkArticle', (articlePath) => {
-    const header = '.Button-heading-Style';
-    cy.visit(articlePath);
-    cy.get(bookmarks.bookmarkBlockButton).click();
-    cy.get(header).first().should('have.text', bookmarks.bookmarkedText);
-});
+Cypress.Commands.add("bookmarkArticle", articlePath => {
+  const header = ".Button-heading-Style"
+  cy.visit(articlePath)
+  cy.get(bookmarks.bookmarkBlockButton).click()
+  cy.get(header)
+    .first()
+    .should("have.text", bookmarks.bookmarkedText)
+})
