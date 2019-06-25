1f545019b5870487cc97b33037dc3205c048f178
fahmed <fahmed@worth.systems>
2019-06-25 14:42:03 +0100

    ONS-177 Changed hard coded values to variables.

--- site/cypress/integration/ui_tests/01-field-officer-navigation.spec.js
+++ site/cypress/integration/ui_tests/01-field-officer-navigation.spec.js
@@ -116,40 +116,41 @@ describe("The field officer flow", function() {
     });
 
     it('Most recent pagination', function () {
+        const textAppearancne = 'text-decoration';
+        const underline = /underline/
         // go to recently updated page
         cy.get(menu.menuButton).contains('Menu').click();
         cy.get(menu.menuLink).contains('Recently updated').click();
 
         // page 1 should be selected. Clicking it again should do nothing.
-        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
-        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
+        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
         cy.get(pagination.pagination1).click();
-        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
-        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
+        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
 
         // clicking page 2 should change switch selected pagination element
         cy.get(pagination.pagination2).click();
-        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('match', /underline/)
-        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('match', underline)
+        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('not.match', underline)
 
         // clicking back takes us back to 1 being selected
         cy.get(pagination.back).click();
-        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
-        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
+        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
         cy.get(pagination.back).should('not.be.visible');
 
         // clicking next takes us forward to 2 being selected
         cy.get(pagination.next).click();
-        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('match', /underline/)
-        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('match', underline)
+        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('not.match', underline)
 
         // after clicking next a couple more times, clicking first still takes us back to 1 being selected.
         cy.get(pagination.next).click();
         cy.get(pagination.next).click();
         cy.get(pagination.first).click();
-        cy.get(pagination.pagination1).should('have.css', 'text-decoration').and('match', /underline/)
-        cy.get(pagination.pagination2).should('have.css', 'text-decoration').and('not.match', /underline/)
+        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
+        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
         cy.get(pagination.first).should('not.be.visible');
      });
-
 });
