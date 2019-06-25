cd3bee18cce28fe6aa17d3957c857378b32d17be
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-06-25 16:40:18 +0100

    unittest for new feedback module

---
+++ site/src/utils/__tests__/feedback.js
@@ -0,0 +1,22 @@
+import Feedback from "../feedback"
+
+describe("Feedback", () => {
+  beforeEach(() => {
+    window._paq = []
+  });
+
+  it("article Is Useful", () => {
+  	Feedback.articleIsUseful("goodArticleName")
+    expect(window._paq).toEqual([['trackEvent', "article-feedback", "positive", "goodArticleName", ""]])
+  })
+
+  it("article Is Not Useful", () => {
+  	Feedback.articleIsNotUseful("badArticleName", "poor spelling")
+    expect(window._paq).toEqual([['trackEvent', "article-feedback", "negative", "badArticleName", "poor spelling"]])
+  })
+
+  it("custom event", () => {
+    Feedback.feedbackEvent("custom", "freeform", "key", "value")
+    expect(window._paq).toEqual([['trackEvent', "custom", "freeform", "key", "value"]])
+  })
+})
