8627c5589ac04e0477a9800d00b7156e66204720
Ian Homer <ihomer@worth.systems>
2019-06-26 19:39:32 +0100

    ONS-9 : Update SSO param binding

--- pipeline/tasks/task-scripts/deploy-site-pre-requisites.sh
+++ pipeline/tasks/task-scripts/deploy-site-pre-requisites.sh
@@ -2,7 +2,7 @@
 
 # Pre-requisites to fail task early
 if [[ "${PROTECTED}" == "true" ]] ; then
-  for MANDATORY in DOMAIN_NAME IDP_ENTRY_POINT ; do
+  for MANDATORY in COOKIE_SECRET DOMAIN_NAME IDP_ENTRY_POINT ; do
     if [[ -z "${!MANDATORY}" ]] ; then
       echo "ERROR : Task parameter ${MANDATORY} MUST be set"
       TASK_ERROR=y
