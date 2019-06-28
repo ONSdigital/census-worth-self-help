f39e258e7587c28fa3c51561b22b9fe19ced8300
fahmed <fahmed@worth.systems>
2019-06-28 10:25:32 +0100

    Added some tests, including refactoring.

--- site/cypress/fixtures/pages/homepagePage.js
+++ site/cypress/fixtures/pages/homepagePage.js
@@ -1,5 +1,11 @@
 module.exports = {
     articleCard: `[data-testid='articletab-article-card']`,
+    exploreContent: `svg[data-icon='book']`,
+    footerOnsMessage: `.Footer-Style`,
+    footerLogo: 'data-testid="footer-logo"',
+    headerTitleSelfHelpFacility: `.Header-Title-Style`,
     homepageTitle: `[data-test='mainLogo__title']`,
-    homepageLogo: `[data-test='ons-logo__header']`
+    homepageLogo: `[data-test='ons-logo__header']`,
+    sectionHeader: `.Section-heading-Style`,
+    viewAll: `.Button-subhead-Style`
 };
--- site/cypress/integration/ui_tests/01-field-officer.spec.js
+++ site/cypress/integration/ui_tests/01-field-officer.spec.js
@@ -22,24 +22,57 @@ const firstArticlePath = '/deep-article';
 const searchText = 'injection';
 const incompleteSearch = searchText.slice(0, -1);
 
-describe("The field officer flow", function() {
-    beforeEach(function() {
+describe("General ONS flow", function() {
+    beforeEach(function () {
         cy.visit('');
         cy.get(homepage.homepageLogo).should('be.visible');
+        // cy.location().should((loc) => {
+        //     expect(loc.protocol).to.eq('https:')
+        // });
     });
 
-    it('Reading an article', function() {
+    it('The correct elements of the homepage are visible', function () {
+        cy.get(homepage.headerTitleSelfHelpFacility).should('have.text', 'Self Help Facility');
+        cy.get(search.searchButton).should('be.visible');
+        cy.get(menu.menuButton).should('be.visible');
+        cy.get(homepage.exploreContent).should('be.visible');
+        cy.get(homepage.sectionHeader).contains('RECENTLY UPDATED').should('be.visible');
+        cy.get(homepage.sectionHeader).contains('MY BOOKMARKS').should('be.visible');
+        cy.get(homepage.viewAll).first().contains('View all >').should('be.visible');
+        cy.get(homepage.footerOnsMessage).contains('© Office for National Statistics 2019').should('be.visible');
+    });
+
+    it('Reading an article', function () {
         cy.get(homepage.articleCard).first().click();
         cy.get(article.content).should('be.visible');
     });
 
-    it('Opening the menu then closing it', function() {
+    it('Exploring content from an article to get to the Root page and dive into another section', function () {
+        const firstOption = '#react-select-2-option-0';
+        cy.visit(firstArticlePath);
+        cy.get('.Button-subhead-Style').first().click();
+        cy.get(firstOption).click();
+        cy.url().should('include', '/explore');
+        cy.get(homepage.articleCard).contains('Format Tests').click();
+        cy.url().should('include', '/format-tests');
+    });
+
+    it('An image in an article', function () {
+        const imageArticleUrlPath = '/image-tests';
+        cy.visit(imageArticleUrlPath);
+        cy.url().should('include', imageArticleUrlPath);
+        cy.get(`[title='image title']`).then(($el) => {
+          Cypress.dom.isVisible($el)
+        })
+    });
+
+    it('Opening the menu then closing it', function () {
         cy.get(menu.menuOverlay).should('not.be.visible');
         cy.get(menu.menuButton).contains('Menu').click();
         cy.get(menu.menuOverlay).should('be.visible');
     });
 
-    it('A search result that won\'t match any articles', function() {
+    it('A search result that won\'t match any articles', function () {
         cy.get(search.searchButton).click();
         cy.get(search.searchResultTitle).contains(beginTypingToSearchTitle);
         cy.get(search.searchBarField).type(searchText.slice(0, -1));
@@ -95,12 +128,6 @@ describe("The field officer flow", function() {
         cy.get(search.searchResultTitle).should('have.text', 'My Bookmarks');
     });
 
-    it('The field officer should all recently updated articles when they click \'view all\'', function () {
-        cy.get(`[href='${mostRecentPage.mostRecentUrlPath}']`).click();
-        cy.url().should('include', mostRecentPage.mostRecentUrlPath);
-        cy.get(search.searchResultTitle).should('have.text', 'Recently Updated');
-    });
-
     it('The field officer should see related articles', function () {
         const editorialWorkflowArticle = 'editorial workflow';
         const editorialWorkflowPath = '/editorial-workflow';
@@ -116,41 +143,41 @@ describe("The field officer flow", function() {
     });
 
     it('Most recent pagination', function () {
-        const textAppearancne = 'text-decoration';
-        const underline = /underline/
+        const textAppearance = 'text-decoration';
+        const underline = /underline/;
         // go to recently updated page
         cy.get(menu.menuButton).contains('Menu').click();
         cy.get(menu.menuLink).contains('Recently updated').click();
 
         // page 1 should be selected. Clicking it again should do nothing.
-        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
-        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
+        cy.get(pagination.pagination1).should('have.css', textAppearance).and('match', underline);
+        cy.get(pagination.pagination2).should('have.css', textAppearance).and('not.match', underline);
         cy.get(pagination.pagination1).click();
-        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
-        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
+        cy.get(pagination.pagination1).should('have.css', textAppearance).and('match', underline);
+        cy.get(pagination.pagination2).should('have.css', textAppearance).and('not.match', underline);
 
         // clicking page 2 should change switch selected pagination element
         cy.get(pagination.pagination2).click();
-        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('match', underline)
-        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('not.match', underline)
+        cy.get(pagination.pagination2).should('have.css', textAppearance).and('match', underline);
+        cy.get(pagination.pagination1).should('have.css', textAppearance).and('not.match', underline);
 
         // clicking back takes us back to 1 being selected
         cy.get(pagination.back).click();
-        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
-        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
+        cy.get(pagination.pagination1).should('have.css', textAppearance).and('match', underline);
+        cy.get(pagination.pagination2).should('have.css', textAppearance).and('not.match', underline);
         cy.get(pagination.back).should('not.be.visible');
 
         // clicking next takes us forward to 2 being selected
         cy.get(pagination.next).click();
-        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('match', underline)
-        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('not.match', underline)
+        cy.get(pagination.pagination2).should('have.css', textAppearance).and('match', underline);
+        cy.get(pagination.pagination1).should('have.css', textAppearance).and('not.match', underline);
 
         // after clicking next a couple more times, clicking first still takes us back to 1 being selected.
         cy.get(pagination.next).click();
         cy.get(pagination.next).click();
         cy.get(pagination.first).click();
-        cy.get(pagination.pagination1).should('have.css', textAppearancne).and('match', underline)
-        cy.get(pagination.pagination2).should('have.css', textAppearancne).and('not.match', underline)
+        cy.get(pagination.pagination1).should('have.css', textAppearance).and('match', underline);
+        cy.get(pagination.pagination2).should('have.css', textAppearance).and('not.match', underline);
         cy.get(pagination.first).should('not.be.visible');
-     });
+    });
 });
