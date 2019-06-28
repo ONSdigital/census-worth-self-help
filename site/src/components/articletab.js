53a96c37d8d3a2599c53642e7d2364320067b514
rmohammed999 <rmohammed@worth.systems>
2019-06-28 09:52:46 +0100

    ONS-124 : added tracking code

--- site/src/components/articletab.js
+++ site/src/components/articletab.js
@@ -14,7 +14,12 @@ export default ({ node }) => {
   let time_ago = moment(node.frontmatter.date).fromNow()
   return (
     <div
-      onClick={() => navigate("/" + link)}
+      onClick={() => 
+        {
+          window._paq.push(['trackEvent', 'Article', 'clicked', title]);
+          return navigate("/" + link)
+        }
+      }
       css={css`
         ${spacing.tab};
         box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
