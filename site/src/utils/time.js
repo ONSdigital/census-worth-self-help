8dbe11deb298fbd3670279847a599f4c49517a6a
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-02 16:36:52 +0100

    added more sensible text for articles with future date on them

---
+++ site/src/utils/time.js
@@ -0,0 +1,9 @@
+const moment = require("moment")
+
+export const getTimeAgoPublished = (time) => {
+	time = moment(time)
+	if (time.isAfter() ){
+		return "Just published"
+	}
+	return time.fromNow()
+}
