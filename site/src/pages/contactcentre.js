f89f961dffa8be2a280a5fde550e017f4314d038
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-04 10:03:20 +0100

    added contact centre to cms, made sure updates happen without refresh

--- site/src/pages/contactcentre.js
+++ site/src/pages/contactcentre.js
@@ -12,59 +12,56 @@ import BlockStatus from "../components/blockstatus"
 import BlockButton from "../components/blockbutton"
 import Section from "../components/section"
 
-export default class CallCentre extends React.Component {
-  constructor(props) {
-    super(props)
-    this.suggestedEdges = CallCentre.getSuggestedEdges(props.data.allMarkdownRemark.edges)
-    this.waitingTime = props.data.markdownRemark ? props.data.markdownRemark.frontmatter.contact_centre_wait_time : 15
-    this.phoneNumber = props.data.markdownRemark ? props.data.markdownRemark.frontmatter.contact_centre_number : "01234 56789"
-
-  }
-
-  static getSuggestedEdges(edges) {
-    return edges
-      .filter(edge => {
-        return (
-          edge.node.frontmatter.tags &&
-          edge.node.frontmatter.tags.includes("popular")
-        )
-      })
-      .slice(0, 3)
-  }
-
-  render() {
-    return (
-      <Layout>
-        <PageTitle>
-          Census Field Support
-        </PageTitle>
-        <TextBlock>
-          Most recent changes, need to check with Phil to see if there's
-          actually content for this
-        </TextBlock>
-        {this.suggestedEdges.length > 0 && (
-          <TabList title="HAVE YOU TRIED..." elements={this.suggestedEdges} />
-        )}
-        <TabList title="CONTACTING FIELD SUPPORT" elements={[]} />
-        <BlockStatus
-          icon={<FontAwesomeIcon icon={faClock} />}
-          data-testid="contact-centre-time-message"
-          title={this.waitingTime + " minutes"}
-          subtitle="Current waiting time"
-        />
-        <Section>
-          <a href={"tel:" + this.phoneNumber}>
-            <BlockButton
-              icon={<FontAwesomeIcon icon={faPhone} />}
-              title="Call Census field support"
-              subtitle={this.phoneNumber}
-            />
-          </a>
-        </Section>
-      </Layout>
-    )
-  }
+const getSuggestedEdges = (edges) => {
+  return edges
+    .filter(edge => {
+      return (
+        edge.node.frontmatter.tags &&
+        edge.node.frontmatter.tags.includes("popular")
+      )
+    })
+    .slice(0, 3)
 }
+
+export default ({ data }) => {
+
+  let suggestedEdges = getSuggestedEdges(data.allMarkdownRemark.edges)
+
+  let waitingTime = data.markdownRemark ? data.markdownRemark.frontmatter.contact_centre_wait_time : 15
+  let phoneNumber = data.markdownRemark ? data.markdownRemark.frontmatter.contact_centre_number : "01234 56789"
+
+  return (
+    <Layout>
+      <PageTitle>
+        Census Field Support
+      </PageTitle>
+      <TextBlock>
+        Most recent changes, need to check with Phil to see if there's
+        actually content for this
+      </TextBlock>
+      {suggestedEdges.length > 0 && (
+        <TabList title="HAVE YOU TRIED..." elements={suggestedEdges} />
+      )}
+      <TabList title="CONTACTING FIELD SUPPORT" elements={[]} />
+      <BlockStatus
+        icon={<FontAwesomeIcon icon={faClock} />}
+        data-testid="contact-centre-time-message"
+        title={waitingTime + " minutes"}
+        subtitle="Current waiting time"
+      />
+      <Section>
+        <a href={"tel:" + phoneNumber}>
+          <BlockButton
+            icon={<FontAwesomeIcon icon={faPhone} />}
+            title="Call Census field support"
+            subtitle={phoneNumber}
+          />
+        </a>
+      </Section>
+    </Layout>
+  )
+}
+
 
 export const query = graphql`
   query {
