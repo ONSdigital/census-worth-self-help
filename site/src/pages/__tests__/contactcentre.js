556d63fe287aaaa09e7ee260ea77839f331f3ee4
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-04 10:16:38 +0100

    added unittests

---
+++ site/src/pages/__tests__/contactcentre.js
@@ -0,0 +1,20 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import ContactCentre from "../contactcentre"
+import { render } from "react-testing-library"
+import { articleList } from "../../utils/testdata"
+
+describe("Contact centre", () => {
+  it("renders correctly without data", () => {
+    const data = {allMarkdownRemark : { edges : []}}
+    const tree = renderer.create(<ContactCentre data={data}/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("renders correctly with data", () => {
+    const data = { allMarkdownRemark : articleList,
+    	markdownRemark : { frontmatter: {contact_centre_wait_time: "25", contact_centre_number: "01111 333333" }} }
+    const tree = renderer.create(<ContactCentre data={data}/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
