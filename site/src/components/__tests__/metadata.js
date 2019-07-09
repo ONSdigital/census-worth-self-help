73ccccb9a0679183dfe80984f40daebddd27ed3a
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-09 16:52:32 +0100

    updated metadata test

--- site/src/components/__tests__/metadata.js
+++ site/src/components/__tests__/metadata.js
@@ -4,17 +4,17 @@ import renderer from "react-test-renderer"
 import Metadata from "../metadata"
 
 describe("Metadata", () => {
-  it("renders correctly with title", () => {
+  it("doesnt error and creates no element with title", () => {
     const tree = renderer
       .create(<Metadata>a title</Metadata>)
       .toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(tree).toBeNull()
   })
 
-  it("renders correctly without a title", () => {
+  it("doesnt error and creates no element without title", () => {
     const tree = renderer
       .create(<Metadata></Metadata>)
       .toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(tree).toBeNull()
   })
 })
