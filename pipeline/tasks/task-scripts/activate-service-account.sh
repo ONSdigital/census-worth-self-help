1f97da96660ad2d06fadf1314ce35f28267f2261
Ian Homer <ihomer@worth.systems>
2019-07-10 16:52:46 +0100

    ONS-214 : Clean up old versions

---
+++ pipeline/tasks/task-scripts/activate-service-account.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set +x
+echo ${BACKEND_CONTENT_REPO}
+echo ${AUTH_KEY} > encrypted_key.txt
+set -x
+base64 -d encrypted_key.txt > service-account.json
+gcloud auth activate-service-account --key-file service-account.json
