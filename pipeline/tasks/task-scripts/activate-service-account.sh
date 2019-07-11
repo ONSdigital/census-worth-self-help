9e1e4a57768d352e0b4046ef5cbdfede1d5e51ec
Ian Homer @ Worth <50905401+ianworth@users.noreply.github.com>
2019-07-11 10:56:36 +0100

    ONS-214 : Clean up old versions (#117)

---
+++ pipeline/tasks/task-scripts/activate-service-account.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set +x
+echo ${BACKEND_CONTENT_REPO}
+echo ${AUTH_KEY} > encrypted_key.txt
+set -x
+base64 -d encrypted_key.txt > service-account.json
+gcloud auth activate-service-account --key-file service-account.json
---
+++ pipeline/tasks/task-scripts/clean-up-old-appengine-versions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+keepCount=${2:-"5"}
+
+function deleteOldVersions() {
+  _service=${1}
+  _keepCount=${2}
+
+  echo "Keeping last ${_keepCount} of app engine service ${_service}"
+  count=0
+  deletedCount=0
+  for version in `gcloud app versions list --service ${_service} --sort-by '~version.id' --format 'value(version.id)'`
+  do
+    ((count++))
+    if [[ ${count} -gt ${_keepCount} ]] ; then
+      echo "Service ${_service} - deleting version ${version}"
+      gcloud app versions delete ${version} --service ${_service} -q
+      ((deletedCount++))
+    else
+      echo "Service ${_service} - keeping  version ${version}"
+    fi
+  done
+  echo "Deleted ${deletedCount} of ${count} versions of service ${_service}"
+}
+
+if [[ -z ${1} ]] ; then
+  totalVersionCount=`gcloud app versions list | wc -l`
+  echo "Pruning old versions from all app engine services"
+  echo "Total version count across all services : ${totalVersionCount}"
+
+  for service in `gcloud app versions list --hide-no-traffic --format 'value(service)'`
+  do
+    deleteOldVersions ${service} ${keepCount}
+  done
+else
+  service=${1:-"default"}
+  deleteOldVersions ${service} ${keepCount}
+fi
+
+
