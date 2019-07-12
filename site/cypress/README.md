14f773ac5bcc7272899adbd3d5ceb13a8aa09972
Fahad <fahmed@worth.systems>
2019-07-12 10:14:14 +0100

    ONS-244 Added request test.

--- site/cypress/README.md
+++ site/cypress/README.md
@@ -1,1 +1,8 @@
-Cypress scripts are prefixed with cy:
+In order to run individual spec files, you can run:
+    `./node_modules/.bin/cypress run --spec cypress/integration/ui_tests/<file_name>`
+    
+There are scripts in package.json to run with either the test runner or headless:
+    `npm run cy:open` - Test runner
+    `npm run cy:run` - Headless
+    
+You can place an 'x' before a test suite or case to skip it e.g. `xdescribe` or `xtest`
---
+++ site/cypress/integration/ui_tests/07-request-secured-site.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+// To run this test, remove the 'x' in front of describe. Placing an x before a test suite or case, will cause it to be skipped
+xdescribe("Request the secured homepage with a valid cookie to check for a 200 status code and the homepage is available", function() {
+    it('SSO request 2', function () {
+        cy.request({
+            method: 'GET',
+            url: 'https://secured.worth.census-gcp.onsdigital.uk/',
+            headers: {
+                authority: 'secured.worth.census-gcp.onsdigital.uk',
+                pragma: 'no-cache',
+                cache: 'no-cache',
+                // upgrade-insecure-requests: 1,
+                userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+                accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                referer: 'https://accounts.google.com/o/saml2/idp?from_login=1&zt=ChRxci1QWGZXU2JwdG91d3g0UGNZbhIfQThiNzVVUGVTVlVSMEVBN1JaNXdOM05OVFNFR3ZoWQ%E2%88%99AJDr988AAAAAXShUFP2zpdyswtB2o6Nv54ZljuQ6RzlV&as=9tVWcb0iIfG2QGX3xZqomw&authuser=0',
+                encoding: 'gzip, deflate, br',
+                language: 'en-GB,en-US;q=0.9,en;q=0.8',
+                // In the keybase repo, grab the cookie from the secret in cypress/secrets.txt
+                cookie: ''
+            },
+        })
+            .then((resp) => {
+                expect(resp.status).to.eq(200);
+                expect(resp.body).to.have.string('RECENTLY UPDATED');
+            });
+    });
+});
+
+
