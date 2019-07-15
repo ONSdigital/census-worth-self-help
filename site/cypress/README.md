124e0f919402dffec236bc5f3ab4adbbc6f11331
Fahad <fahmed@worth.systems>
2019-07-15 13:32:41 +0100

    Added the no protocol secret

--- site/cypress/README.md
+++ site/cypress/README.md
@@ -4,5 +4,19 @@ In order to run individual spec files, you can run:
 There are scripts in package.json to run with either the test runner or headless:
     `npm run cy:open` - Test runner
     `npm run cy:run` - Headless
-    
+You can also add `--browser=chrome` if you want to run a test without opening the test runner:
+    `./node_modules/.bin/cypress run --browser=chrome --spec <path/to/file>`
+
 You can place an 'x' before a test suite or case to skip it e.g. `xdescribe` or `xtest`
+
+In order to run the secured_request test, you need to create a symbolic link in the keybase repo (make sure localhost is
+running, as the baseURL property in cypress.json will always look for localhost):
+    
+    
+    `cd into the site folder in your ONS project
+    ln ~/secrets/cypress/cypress_env_variables.sh
+    npm run cy:secured:test`
+       
+To get a HTML report, after a test has finished and JSON file has been created as the report, then run:
+    `npm run merge:reports`
+This will compile all JSON report files into a single HTML file, to open in a browser.
--- site/cypress/integration/ui_tests/secured_request/01-request-secured-site.js
+++ site/cypress/integration/ui_tests/secured_request/01-request-secured-site.js
@@ -1,14 +1,12 @@
 /// <reference types="Cypress" />
 
-// To run this test, remove the 'x' in front of describe. Placing an x before a test suite or case, will cause it to be skipped
 describe("Request the secured homepage with a valid cookie to check for a 200 status code and the homepage is available", function() {
     it('SSO request 2', function () {
-        cy.log(Cypress.env('SECURED_URL'));
         cy.request({
             method: 'GET',
             url: Cypress.env('SECURED_URL'),
             headers: {
-                authority: 'secured.worth.census-gcp.onsdigital.uk',
+                authority: Cypress.env('CYPRESS_SECURED_URL_NO_PROTOCOL'),
                 pragma: 'no-cache',
                 cache: 'no-cache',
                 userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
@@ -16,7 +14,6 @@ describe("Request the secured homepage with a valid cookie to check for a 200 st
                 referer: 'https://accounts.google.com/o/saml2/idp?from_login=1&zt=ChRxci1QWGZXU2JwdG91d3g0UGNZbhIfQThiNzVVUGVTVlVSMEVBN1JaNXdOM05OVFNFR3ZoWQ%E2%88%99AJDr988AAAAAXShUFP2zpdyswtB2o6Nv54ZljuQ6RzlV&as=9tVWcb0iIfG2QGX3xZqomw&authuser=0',
                 encoding: 'gzip, deflate, br',
                 language: 'en-GB,en-US;q=0.9,en;q=0.8',
-                // In the keybase repo, grab the cookie from the secret in cypress/secrets.txt
                 cookie: Cypress.env('COOKIE')
             },
         })
