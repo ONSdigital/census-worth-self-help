1d844e4f14082aaac0ce3680584501daf25678ec
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-17 15:15:48 +0100

    ONS-259 prevent xss attacks from content

--- site/src/utils/__tests__/sourcetransforms.js
+++ site/src/utils/__tests__/sourcetransforms.js
@@ -1,6 +1,6 @@
 process.env.GATSBY_ASSETS_PATH = "test"
 
-import { transformSources } from "../sourcetransforms"
+import { transformSources, htmlSanitize } from "../sourcetransforms"
 
 describe("transformSources", () => {
 
@@ -10,5 +10,35 @@ describe("transformSources", () => {
 
   it("doesn't alter html without tags", () => {
   	expect(transformSources("wibble flibble wibble")).toEqual("wibble flibble wibble")
+  })
+})
+
+describe("htmlSanitize", () => {
+
+  it("no alteration to plain string", () => {
+  	expect(htmlSanitize("this is my very normal string")).toEqual("this is my very normal string")
+  })
+
+  it("no alteration to string containing allowed elements", () => {
+  	expect(htmlSanitize('<h1>Hi</h1><b>bold</b><blockquote>bq</blockquote>')).toEqual("<h1>Hi</h1><b>bold</b><blockquote>bq</blockquote>")
+  })
+
+  it("no alteration to images, video or audio", () => {
+  	expect(htmlSanitize('<video controls><source src="/my-video.mp4" type="video/mp4">msg</video>'))
+  		.toEqual('<video controls><source src="/my-video.mp4" type="video/mp4"></source>msg</video>')
+
+  	expect(htmlSanitize('<audio controls><source src="/my-audio.mp3" type="audio/mp3">msg</audio>'))
+  		.toEqual('<audio controls><source src="/my-audio.mp3" type="audio/mp3"></source>msg</audio>')
+
+  	expect(htmlSanitize('<img src="/test" />'))
+  		.toEqual('<img src="/test" />')
+  })
+
+  it("dissallowed tags", () => {
+  	expect(htmlSanitize("hi <script>alert('xss');</script> hi")).toEqual("hi  hi")
+
+  	expect(htmlSanitize("made up:<wibble>wibble</wibble>")).toEqual("made up:wibble")
+
+  	expect(htmlSanitize("<b onmouseover=alert(‘attribute attack‘)>hi</b>")).toEqual("<b>hi</b>")
   })
 })
--- site/src/utils/sourcetransforms.js
+++ site/src/utils/sourcetransforms.js
@@ -1,3 +1,5 @@
+var sanitizeHtml = require('sanitize-html');
+
 export const transformSources = htmlString => {
   let videoPath = process.env.GATSBY_ASSETS_PATH
     ? process.env.GATSBY_ASSETS_PATH
@@ -6,4 +8,17 @@ export const transformSources = htmlString => {
     return ""
   }
   return htmlString.replace(/{{TARGET_ASSETS_SRC}}/g, videoPath)
+}
+
+export const htmlSanitize = htmlString => {
+  return sanitizeHtml(htmlString, {
+  	allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'video', 'audio', 'img', 'source', 'h1', 'h2' ]),
+  	allowedAttributes: {
+  		a: [ 'href', 'name', 'target' ],
+  		img: [ 'src' ],
+  		source: [ 'src', 'type'],
+  		video: ['controls'],
+  		audio: ['controls']
+  	}
+  })
 }
