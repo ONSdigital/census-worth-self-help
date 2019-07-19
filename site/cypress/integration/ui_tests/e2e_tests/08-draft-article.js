7b0fa247750a97398c565db82a81fc472591f8ba
Fahad <fahmed@worth.systems>
2019-07-19 11:28:25 +0100

    ONS-232 Removed unneeded conditional.

--- site/cypress/integration/ui_tests/e2e_tests/08-draft-article.js
+++ site/cypress/integration/ui_tests/e2e_tests/08-draft-article.js
@@ -5,9 +5,8 @@ const article = require('../../../fixtures/fragments/article');
 
 describe("Draft articles", function() {
     it('An article with the \'draft\' tag should not be visible [ONS-232]', function () {
-        if (cy.exec('echo $EXCLUDE_DRAFTS').its('stdout').should('eq', 'true')) {
-            cy.visit('/draft-test-article');
-            cy.get(article.content).should('not.be.visible');
-        }
+        cy.exec('echo $EXCLUDE_DRAFTS').its('stdout').should('eq', 'true');
+        cy.visit('/draft-test-article');
+        cy.get(article.content).should('not.be.visible');
     });
 });
