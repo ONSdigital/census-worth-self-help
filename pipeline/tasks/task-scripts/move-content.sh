4983d06001bb95ab34c95984625b902e7dcc6bd5
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-25 15:08:52 +0100

    ONS-284 restricted filetypes being transferred on box

---
+++ pipeline/tasks/task-scripts/move-content.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+BUILD="_build/content/"
+mkdir -p "$BUILD" && cp -R ../../content-repository/content/* "$BUILD"
+ASSET_DIR="static/assets/"
+
+#cp -R ../../content-repository/static/assets/ "$ASSET_DIR"
+find ../../content-repository/static/assets \
+	-type f \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.png" -or -name "*.gif" \) -print0  \
+| xargs -0 -I filepath cp -prv filepath "$ASSET_DIR"
