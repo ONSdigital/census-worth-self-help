bd7915ff6fc85c5f95f47c4ca5d09a139791d13f
rmohammed999 <rmohammed@worth.systems>
2019-07-25 10:28:50 +0100

    ONS-231 : cleanup

--- site/app/handlers.js
+++ site/app/handlers.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcrypt')
 const crypto = require('crypto');
 
 const destinationRegex = /^[a-z0-9-]+$/
@@ -23,8 +22,6 @@ const extractArticleName = function (path) {
   return false
 }
 
-// var validSecret = bcrypt.hash(Date.now() + COOKIE_SECRET, 10)
-
 module.exports = {
   // User serialisation / deserialisation is simple one-to-one mappin
   mapUser : function (user, done) {
@@ -36,9 +33,7 @@ module.exports = {
   },
 
   createValidSecret : function ()  {
-    var validSecret = crypto.createCipher("aes-256-ctr", COOKIE_SECRET).update(Date.now().toString(), "utf-8", "hex");
-    console.log(validSecret)
-    return validSecret
+    return crypto.createCipher("aes-256-ctr", COOKIE_SECRET).update(Date.now().toString(), "utf-8", "hex");
   },
 
   logout: function(idpLogout) {
@@ -59,7 +54,6 @@ module.exports = {
       decodedSecret = crypto.createDecipher("aes-256-ctr", COOKIE_SECRET).update(secret, "hex", "utf-8")
       return ((Date.now().toString() - decodedSecret) / (60 * 1000) < 120)
     }
-
 
     if (req.isAuthenticated() && validateToken(req.user.secret)) {
       next()
