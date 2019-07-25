25543b17674a3b92f7c08c1e2c94ae3f09b043e5
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-25 16:28:27 +0100

    updated comment

--- pipeline/tasks/task-scripts/move-content.sh
+++ pipeline/tasks/task-scripts/move-content.sh
@@ -6,7 +6,8 @@ BUILD="_build/content/"
 mkdir -p "$BUILD" && cp -R ../../content-repository/content/* "$BUILD"
 ASSET_DIR="static/assets/"
 mkdir $ASSET_DIR
-#cp -R ../../content-repository/static/assets/ "$ASSET_DIR"
+
+# Copy only image extensions into the asset directory
 find ../../content-repository/static/assets \
   -type f \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.png" -or -name "*.gif" \) -print0  \
 | xargs -0 -I filepath cp -prv filepath "$ASSET_DIR"
