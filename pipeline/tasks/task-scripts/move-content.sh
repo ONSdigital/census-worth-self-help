c31db516644781f8d12e2072d7c2d097076d0cca
Nathan Phillips <nathanphillips@MacBook-Pro-5.local>
2019-07-25 15:55:09 +0100

    create directory before cp'ing to it

--- pipeline/tasks/task-scripts/move-content.sh
+++ pipeline/tasks/task-scripts/move-content.sh
@@ -5,7 +5,7 @@ set -e
 BUILD="_build/content/"
 mkdir -p "$BUILD" && cp -R ../../content-repository/content/* "$BUILD"
 ASSET_DIR="static/assets/"
-
+mkdir $ASSET_DIR
 #cp -R ../../content-repository/static/assets/ "$ASSET_DIR"
 find ../../content-repository/static/assets \
   -type f \( -name "*.jpg" -or -name "*.jpeg" -or -name "*.png" -or -name "*.gif" \) -print0  \
