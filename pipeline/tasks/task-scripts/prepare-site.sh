6dbf1403c7930520e1bdf351bab8cc8f005c4555
Ian Homer @ Worth <50905401+ianworth@users.noreply.github.com>
2019-07-27 13:57:59 +0100

    ONS-231 : Inject SSO timeout env into appengine env (#180)

--- pipeline/tasks/task-scripts/prepare-site.sh
+++ pipeline/tasks/task-scripts/prepare-site.sh
@@ -45,11 +45,13 @@ if [[ "${SP_PROTECTED}" != "false" ]] ; then
   set +x
   echo "  COOKIE_SECRET: '${COOKIE_SECRET}'" >> ${appFile}
   set -x
+  echo "  COOKIE_TIMEOUT: ${COOKIE_TIMEOUT}" >> ${appFile}
   echo "  IDP_ENTRY_POINT: '${IDP_ENTRY_POINT}'" >> ${appFile}
   echo "  IDP_LOGOUT: '${IDP_LOGOUT}'" >> ${appFile}
   echo "  SP_CALLBACK_URL: '${spProtocol}://${SP_DOMAIN_NAME}/sso/callback'" >> ${appFile}
   echo "  SP_ENTITY_ID: '${spProtocol}://${SP_DOMAIN_NAME}/saml/metadata'" >> ${appFile}
   echo "  SP_PROTECTED: ${SP_PROTECTED}" >> ${appFile}
+  echo "  VALID_TOKEN_AGE: ${VALID_TOKEN_AGE}" >> ${appFile}
 else
   echo "env_variables:" >> ${appFile}
   echo "  SP_PROTECTED: ${SP_PROTECTED}" >> ${appFile}
