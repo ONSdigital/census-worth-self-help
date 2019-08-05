df2b5e4cd273ad8048a89f14215560416b07363b
nphillipsworth <39960458+nphillipsworth@users.noreply.github.com>
2019-08-05 11:28:09 +0100

    ONS-315 ran snapshot update. Deleted unused styles. (#195)

--- site/src/utils/styles.js
+++ site/src/utils/styles.js
@@ -2,24 +2,8 @@ import { css } from "@emotion/core"
 
 export const colors = {
   navy_normal: "rgb(0,61,89)",
-  marigold: "rgb(255, 204, 0)",
-  tangerine: "rgb(255, 149, 0)",
   white: "rgb(255, 255, 255)",
-  black: "rgb(0, 0, 0)",
-  pale_grey: "rgb(239, 239, 244)",
   pale_lilac: "rgb(229, 229, 234)",
-  light_blue_grey: "rgb(209, 209, 214)",
-  light_blue_grey_two: "rgb(199, 199, 204)",
-  blue_grey: "rgb(142, 142, 147)",
-  brownish_grey: "rgb(110, 110, 110)",
-  royal_5: "rgb(10, 10, 120)",
-  dark_slate_blue_7: "rgb(25, 25, 100)",
-  dark_slate_blue_18: "rgb(25, 25, 100)",
-  eggplant_45: "rgb(4, 4, 15)",
-  blue_grey_50: "rgb(142, 142, 147)",
-  dark_navy_blue_22: "rgb(0, 0, 25)",
-  velvet: "rgb(98, 13, 111)",
-  purply: "rgb(170, 59, 188)",
   golden_yellow: "rgb(248, 196, 30)",
   light_tan: "rgb(253, 239, 173)",
   black_two: "rgb(48, 48, 48)",
@@ -47,12 +31,6 @@ export const spacing = {
   `,
   large_vertical: css`
     padding: 30px 0px;
-  `,
-  text_clearance: css`
-    margin: 0px 2px;
-  `,
-  minimum_gap: css`
-    margin: 2px 0px;
   `,
   tab: css`
     padding: 15px;
