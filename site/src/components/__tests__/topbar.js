c3ec88cb4e41df864758c610c3dc8cd721e36a37
nphillipsworth <39960458+nphillipsworth@users.noreply.github.com>
2019-08-06 14:50:13 +0100

    ONS-305 used query param to return to the previous page safely (#196)

--- site/src/components/__tests__/topbar.js
+++ site/src/components/__tests__/topbar.js
@@ -4,6 +4,12 @@ import renderer from "react-test-renderer"
 import TopBar from "../topbar"
 import { render, fireEvent } from "react-testing-library"
 
+import { navigate } from "@reach/router"
+
+jest.mock('@reach/router', () => ({
+  navigate: jest.fn(),
+}))
+
 describe("TopBar", () => {
 
   let updateFunction = jest.fn()
@@ -37,5 +43,37 @@ describe("TopBar", () => {
   	fireEvent.change(searchBox, { target: { value: 'TEST VALUE' } });
 
   	expect(updateFunction).toHaveBeenCalled();
+  })
+
+  it("menu links return to original page", () => {
+
+    // create a fake window object with the pathname parameter and the redirect parameter.
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+      value: {
+        pathname: "testtest",
+        search: "?redirect=" + encodeURIComponent("/testtest/")
+      }
+    });
+
+    // if you are on page testtest and click menu:
+    let { getByText } = render(<TopBar />);
+    
+    const menuButton = getByText('Menu')
+    fireEvent.click(menuButton);
+
+    // you get sent to the menu page with redirect link in the url
+    expect(navigate).toHaveBeenCalledTimes(1)
+    expect(navigate).toHaveBeenCalledWith('/menu/?redirect=testtest')
+
+    // if you have the redirect link in the url and click close
+    getByText = render(<TopBar backButton={true} />).getByText;
+    
+    const closeButton = getByText('Close')
+    fireEvent.click(closeButton);
+
+    // you return to the original page
+    expect(navigate).toHaveBeenCalledTimes(2)
+    expect(navigate).toHaveBeenCalledWith('/testtest/')
   })
 })
