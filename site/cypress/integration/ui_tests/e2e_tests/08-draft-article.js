30e7002c977e469126bb06ad483b0308cc5524aa
Ian Homer <ihomer@worth.systems>
2019-10-31 14:27:49 +0000

    ONS-359 : Ensure test fails before implementing

--- site/cypress/integration/ui_tests/e2e_tests/08-draft-article.js
+++ site/cypress/integration/ui_tests/e2e_tests/08-draft-article.js
@@ -5,12 +5,11 @@ const article = require('../../../fixtures/fragments/article');
 
 describe("Draft articles", function() {
     it('An article with the draftresason field or \'draft\' tag should not be visible [ONS-232] and [ONS-351]', function () {
-        Cypress.env('RETRIES', 2);
         cy.visit('/draft-test-article');
-        cy.get(article.content).should('not.be.visible');
+        cy.get(article.content, { timeout: 0 }).should('not.be.visible');
         cy.visit('/draft-legacy-test-article');
-        cy.get(article.content).should('not.be.visible');
+        cy.get(article.content, { timeout: 0 }).should('not.be.visible');
         cy.visit('/ready-for-production-test-article');
-        cy.get(article.content).should('not.visible');
+        cy.get(article.content).should('be.visible');
     });
 });
--- site/cypress/integration/ui_tests/e2e_tests/09-cc-only-article.js
+++ site/cypress/integration/ui_tests/e2e_tests/09-cc-only-article.js
@@ -5,7 +5,9 @@ const article = require('../../../fixtures/fragments/article');
 
 describe("Call centre only articles", function() {
     it('An article with the cc only flag set should not be visible [ONS-359]', () => {
+        cy.visit('/ready-for-production-test-article');
+        cy.get(article.content).should('be.visible');
         cy.visit('/cc-only-article');
-        cy.get(article.content).should('not.be.visible');
+        cy.get(article.content, { timeout: 0 }).should('not.be.visible');
     });
 });
