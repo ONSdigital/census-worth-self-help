d3882d40e84398e9f1a9c3ffae2a89f33507cce9
Ian Homer <ihomer@worth.systems>
2019-11-11 22:45:20 +0000

    ONS-357 : Report filter on other fields

--- site/src/components/admin/reportItem.js
+++ site/src/components/admin/reportItem.js
@@ -1,6 +1,7 @@
 import React from "react"
+import { css } from "@emotion/core"
 
-export default ({ node }) => {
+export default ({ node, fieldNames }) => {
   let title = node.frontmatter.title
   let link = `/admin/#/collections/${node.fields.collection}/entries/${node.fields.pagename}`
   return (
@@ -8,6 +9,19 @@ export default ({ node }) => {
       <a href={link}>
         {title}
       </a>
+      <div css={css`
+          padding-left:2em;
+          font-size:small;
+          color:#aaa;
+        `}>
+      {fieldNames.map(fieldName => (
+        <div
+          css={css`
+            padding-left:1em;
+          `}
+          id={fieldName}>{fieldName} = {node.frontmatter[fieldName] && node.frontmatter[fieldName].toString()}</div>
+      ))}
+      </div>
     </div>
   )
 }
--- site/src/pages/admin/report.js
+++ site/src/pages/admin/report.js
@@ -1,89 +1,163 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { css } from "@emotion/core"
 import Layout from "../../components/admin/layout"
 import PageTitle from "../../components/pagetitle"
 import TextBlock from "../../components/textblock"
 import ReportItem from "../../components/admin/reportItem"
 
+const NA = "(not set)"
+const SET = "(set)"
+const INITIAL_STATE = {
+  author: "",
+  cconly: "",
+  contentsource: "",
+  department: "",
+  directory: "",
+  draftreason: "",
+  optimisedby: "",
+  role: "",
+  signedby: "",
+  title: ""
+}
 export default class Report extends React.Component {
   constructor(props) {
     super(props)
     this.data = props.data
-    this.state = {
-      role: "",
-      title: ""
+    this.state = INITIAL_STATE
+    this.fieldIncludes = this.fieldIncludes.bind(this)
+    this.fieldEquals = this.fieldEquals.bind(this)
+    this.getCollectionSelect = this.getCollectionSelect.bind(this)
+    this.getCollectionOptions = this.getCollectionOptions.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+
+  updateReport(fieldName) {
+    return (event) => {
+      this.setState({
+        [fieldName]: event.target.value
+      })
     }
-    this.updateReportTitle = this.updateReportTitle.bind(this)
-    this.updateReportRole = this.updateReportRole.bind(this)
   }
 
-  updateReportTitle(event) {
-    this.setState({
-      title : event.target.value.toLowerCase()
-    })
+  reset() {
+    this.setState(INITIAL_STATE);
   }
 
-  updateReportRole(event) {
-    this.setState({
-      role : event.target.value
-    })
+  fieldIncludes(fieldName, node) {
+    return !this.state[fieldName] ||
+      node.frontmatter[fieldName].toLowerCase().includes(this.state[fieldName].toLowerCase())
+  }
+
+  fieldEquals(fieldName, node) {
+    const value = this.state[fieldName]
+    return !value ||
+      `${node.frontmatter[fieldName]}` === value ||
+      (value === NA && !this.isSet(node.frontmatter[fieldName])) ||
+      (value === SET && this.isSet(node.frontmatter[fieldName]))
+  }
+
+  isSet(value) {
+    return value !== null && value !== ""
+  }
+
+
+  getCollectionOptions(fieldName) {
+    if (fieldName === "cconly")
+      return (
+        ["true","false"].map(( value ) => (
+          <option key={value}>{value}</option>
+        ))
+      )
+    if (["author","contentsource", "signedby"].includes(fieldName))
+      return (<span/>)
+    return this.data[fieldName].edges.map(({ node }) => (
+      <option
+        key={fieldName + "-option/" + node.fields.pagename}
+      >{node.frontmatter.title}</option>
+    ))
+  }
+
+  getCollectionSelect(fieldName) {
+    return (<select
+        id={"report-select-" + fieldName}
+        value={this.state[fieldName]}
+        onChange={this.updateReport(fieldName).bind(this)}
+        css={css`
+          margin-right: 1em;
+        `}
+      >
+        <option/>
+        <option>{NA}</option>
+        <option>{SET}</option>
+        {this.getCollectionOptions(fieldName)}
+      </select>
+    )
   }
 
   render() {
+    const stateKeys = Object.keys(this.state).filter(key => this.state[key])
     const items = this.data.allItems.edges
       .filter(({ node }) => {
-        if (this.state.title) {
-          if (!node.frontmatter.title.toLowerCase().includes(this.state.title)) {
-            return false
-          }
-        }
-        if (this.state.role) {
-          if (node.frontmatter.role !== this.state.role) {
-            return false
-          }
-        }
-        return true
-      })
-      .map(({ node }) => (
+        return this.fieldEquals("author", node) &&
+          this.fieldEquals("cconly", node) &&
+          this.fieldEquals("contentsource", node) &&
+          this.fieldEquals("departments", node) &&
+          this.fieldEquals("directory", node) &&
+          this.fieldEquals("draftreason", node) &&
+          this.fieldEquals("optimisedby", node) &&
+          this.fieldEquals("role", node) &&
+          this.fieldEquals("signedby", node) &&
+          this.fieldIncludes("title", node)
+      }).map(({ node }) => (
       <ReportItem
           key={node.fields.collection + "/" + node.fields.pagename}
-          node={node}/>
+          node={node}
+          fieldNames={stateKeys}/>
     ))
 
-    const roles = this.data.allRoles.edges.map(({ node }) => (
-      <option>{node.frontmatter.title}</option>
-    ))
+    const filterAsString = stateKeys
+      .map(key => (
+        <li key={"state/" + key}>{key} = {this.state[key]}</li>
+      ))
 
+    const count = items.length;
     return (
       <Layout>
         <PageTitle>
-          Report
+          CMS content report : {count} item{(count !== 1) && <span>s</span>}
         </PageTitle>
         <TextBlock>
-          CMS content report
+          <ul>{filterAsString}</ul>
+          <div css={css`
+            border: 1px solid #555;
+            padding: 1em;
+          `}>
+            <div>
+              Title <input
+                id="report-title"
+                maxLength="50"
+                size={50}
+                type="text"
+                value={this.state.title}
+                onChange={this.updateReport("title").bind(this)}
+              />
+            </div>
+            Call&nbsp;Centre&nbsp;Only{this.getCollectionSelect("cconly")}
+            Draft&nbsp;Reasons{this.getCollectionSelect("draftreason")}
+            <div>Related&nbsp;Team(s){this.getCollectionSelect("departments")}</div>
+            Content&nbsp;Source{this.getCollectionSelect("contentsource")}
+            Role{this.getCollectionSelect("role")}
+            Directory{this.getCollectionSelect("directory")}
+            Author{this.getCollectionSelect("author")}
+            Optimised&nbsp;By{this.getCollectionSelect("optimisedby")}
+            Signed&nbsp;By{this.getCollectionSelect("signedby")}
+            <div css={css`
+              text-align:right;
+            `}><button css={css`font-size:1.5em;`} onClick={this.reset}>Reset</button></div>
+          </div>
+          {items}
         </TextBlock>
-        <input
-          id="report-title"
-          data-testid="report-title"
-          maxLength="20"
-          type="text"
-          value={this.state.title}
-          onChange={this.updateReportTitle}
-        />
-        <select
-          id="report-role"
-          data-testid="report-role"
-          value={this.state.role}
-          onChange={this.updateReportRole}
-          >
-          <option/>
-          {roles}
-        </select>
-        <input
-          type="checkbox"
-          name="cconly"
-          value={this.state.cconly}/> cc only
-        {items}
       </Layout>
     )
   }
@@ -99,20 +173,92 @@ export const query = graphql`
       edges {
         node {
           ...BaseArticleFields
+          frontmatter {
+            author
+            cconly
+            contentsource
+            departments
+            draftreason
+            optimisedby
+            signedby       
+          }
         }
       }
     }
-    allRoles: allMarkdownRemark(
+    role: allMarkdownRemark(
       sort: { fields: frontmatter___title }
       filter: { fields: { collection: { eq: "role" } } }
     ) {
       edges {
         node {
+          fields {
+            pagename
+          }          
           frontmatter {
             title
           }
         }
       }
-    }    
+    }  
+    draftreason: allMarkdownRemark(
+      sort: { fields: frontmatter___title }
+      filter: { fields: { collection: { eq: "draftreason" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            pagename
+          }          
+          frontmatter {
+            title
+          }
+        }
+      }
+    } 
+    departments : allMarkdownRemark(
+      sort: { fields: frontmatter___title }
+      filter: { fields: { collection: { eq: "departments" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            pagename
+          }          
+          frontmatter {
+            title
+          }
+        }
+      }
+    } 
+    directory : allMarkdownRemark(
+      sort: { fields: frontmatter___title }
+      filter: { fields: { collection: { eq: "directories" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            pagename
+          }          
+          frontmatter {
+            title
+          }
+        }
+      }
+    }       
+    optimisedby : allMarkdownRemark(
+      sort: { fields: frontmatter___title }
+      filter: { fields: { collection: { eq: "optimisedby" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            pagename
+          }          
+          frontmatter {
+            title
+          }
+        }
+      }
+    }  
   }
 `
