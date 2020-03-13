eab0b38557541f1307409008cb47fbcfba626fb3
Henry Penton <hpenton@worth.systems>
2020-03-13 14:38:15 +0000

    ONS-503 upgrading service worker and updating options to work with 3.x

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -6,15 +6,15 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         start_url: `/`,
-        orientation: `portrait`,
-      },
+        orientation: `portrait`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `${__dirname}/_build/content/`,
-      },
+        path: `${__dirname}/_build/content/`
+      }
     },
     {
       resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
@@ -28,86 +28,85 @@ module.exports = {
             author: node => node.frontmatter.author,
             tags: node => node.frontmatter.tags,
             body: node => node.rawMarkdownBody
-          },
+          }
         },
         // Remove .md with no content
         filter: (node, getNode) => {
           return node.rawMarkdownBody.trim() != "" && !excludeDraftArticle(node)
-        },
-      },
+        }
+      }
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-favicon`,
       options: {
-        logo: "./static/favicon.png",
+        logo: "./static/favicon.png"
       }
     },
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        debug: true,
-        globPatterns: ['**/*.{css,png,js,json,html,svg,woff,woff2,ttf}'],
-        runtimeCaching: [
-          {
-            urlPattern: /online/,
-            handler: `networkOnly`
-          },
-          {
-            urlPattern: /api\/.*/,
-            handler: `networkOnly`
-          },
-          {
-            urlPattern: /cms.js$/,
-            handler: `cacheFirst`,
-            options: {
-              cacheName: 'netlify-cms',
-              expiration: {
-                maxAgeSeconds: 3600,
-                purgeOnQuotaError: true,
+        workboxConfig: {
+          debug: true,
+          globPatterns: ["**/*.{css,png,js,json,html,svg,woff,woff2,ttf}"],
+          runtimeCaching: [
+            {
+              urlPattern: /online/,
+              handler: `networkOnly`
+            },
+            {
+              urlPattern: /api\/.*/,
+              handler: `networkOnly`
+            },
+            {
+              urlPattern: /cms.js$/,
+              handler: `cacheFirst`,
+              options: {
+                cacheName: "netlify-cms",
+                expiration: {
+                  maxAgeSeconds: 3600,
+                  purgeOnQuotaError: true
+                }
               }
+            },
+            {
+              urlPattern: /sw.js$/,
+              handler: `staleWhileRevalidate`
+            },
+            {
+              urlPattern: /(\.js$|\.css$|static\/)/,
+              handler: `cacheFirst`
+            },
+            {
+              urlPattern: /.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+              handler: `staleWhileRevalidate`
             }
-          },
-          {
-            urlPattern: /sw.js$/,
-            handler: `staleWhileRevalidate`,
-          },
-          {
-            urlPattern: /(\.js$|\.css$|static\/)/,
-            handler: `cacheFirst`,
-          },
-          {
-            urlPattern: /.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-            handler: `staleWhileRevalidate`,
-          }
-        ]
-      },
+          ]
+        }
+      }
     }
-  ],
+  ]
 }
 
 if (!process.env.DISABLE_NETLIFY) {
-  module.exports.plugins.push(
-  {
+  module.exports.plugins.push({
     resolve: `gatsby-plugin-netlify-cms`,
     options: {
-      modulePath: `${__dirname}/src/cms/cms.js`,
-    },
-  });
+      modulePath: `${__dirname}/src/cms/cms.js`
+    }
+  })
 }
 
 if (process.env.ENABLE_MATOMO) {
-  module.exports.plugins.push(
-    {
-      resolve: 'gatsby-plugin-matomo',
-      options: {
-        siteId: process.env.MATOMO_SITE_ID,
-        matomoUrl: process.env.MATOMO_IP,
-        siteUrl: process.env.MATOMO_URL,
-        // dev: true
-      }
+  module.exports.plugins.push({
+    resolve: "gatsby-plugin-matomo",
+    options: {
+      siteId: process.env.MATOMO_SITE_ID,
+      matomoUrl: process.env.MATOMO_IP,
+      siteUrl: process.env.MATOMO_URL
+      // dev: true
     }
-  );
+  })
 }
