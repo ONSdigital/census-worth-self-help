e2fadd56df96991ee932033ae6a7cab790c095b9
Henry Penton <hpenton@worth.systems>
2020-04-06 16:20:20 +0100

    ONS-616 Adding field support call button analytics

--- site/src/pages/__tests__/contactcentre.js
+++ site/src/pages/__tests__/contactcentre.js
@@ -3,12 +3,15 @@ import renderer from "react-test-renderer"
 import ContactCentre from "../contactcentre"
 import { getSuggestedEdges } from "../contactcentre"
 import { sanitizePhoneNumber } from "../contactcentre"
-import { render } from "react-testing-library"
+import { render, fireEvent } from "react-testing-library"
 import { articleList, popularList } from "../../utils/testdata"
 
 
 describe("Contact centre", () => {
-    
+  beforeEach(() => {
+    window._paq = []
+  })
+
   it("renders correctly without data", () => {
     const data = { allMarkdownRemark: { edges: [] } }
     const tree = renderer.create(<ContactCentre data={data} />).toJSON()
@@ -37,5 +40,32 @@ describe("Contact centre", () => {
     expect(getSuggestedEdges(articleList.edges)).toEqual(
       popularList.edges.slice(0, 3)
     )
+  })
+  it("registers with the analytics object", () => {
+    const data = {
+      allMarkdownRemark: articleList,
+      markdownRemark: {
+        frontmatter: {
+          contact_centre_text: "wibble wibble",
+          contact_centre_number: "01111 333333"
+        }
+      }
+    }
+
+    const { getByTestId } = render(<ContactCentre data={data} />)
+    fireEvent.click(getByTestId("cc-link"))
+
+    expect(window._paq.length).toEqual(2)
+
+    expect(window._paq[0]).toEqual(["setCustomDimension", 1, "other"])
+
+    const expectedArray = [
+      "trackEvent",
+      "census-field-support",
+      "call-field-support",
+      "",
+      ""
+    ]
+    expect(window._paq[1]).toEqual(expectedArray)
   })
 })
