777923b55e1e533e97a9afb508e5114e4363b597
Henry Penton <hpenton@worth.systems>
2020-04-15 10:35:11 +0100

    ONS-639 removing logging

--- analytics/src/fetcher.js
+++ analytics/src/fetcher.js
@@ -4,10 +4,7 @@ class Fetcher {
   fetch(url) {
     return fetch(url)
       .then((res) => {
-        console.log(res.status);
         if (res.ok) {
-
-          console.log(res.text());
           const jsonout = res.json();
           return jsonout;
         } else {
