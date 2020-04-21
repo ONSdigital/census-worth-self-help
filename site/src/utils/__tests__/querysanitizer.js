1ef724b095bb2ec96430042f880e3958ce6d4f9c
Henry Penton <hpenton@worth.systems>
2020-04-21 15:42:31 +0100

    multiple token string

--- site/src/utils/__tests__/querysanitizer.js
+++ site/src/utils/__tests__/querysanitizer.js
@@ -23,14 +23,42 @@ describe("query sanitizer", () => {
     expect(querySanitizer.sanitize(multipleToken)).toEqual(multipleToken)
   })
   test("pass in string of 1 token incorrectly spelt correct it", () => {
-    const querySanitizer = new QuerySanitizer()
+    const querySanitizer = new QuerySanitizer({ yaers: "years" })
     const incorrectToken = "yaers"
     const correctlySpeltToken = "years"
     expect(querySanitizer.sanitize(incorrectToken)).toEqual(correctlySpeltToken)
   })
-  
+  test("If tokenize is given an empty string it returns an empty string", () => {
+    const querySanitizer = new QuerySanitizer()
+    expect(querySanitizer.tokenize("")).toEqual([""])
+  })
+  test("If tokenize is given a single token string it returns an empty string", () => {
+    const querySanitizer = new QuerySanitizer()
+    expect(querySanitizer.tokenize("blah")).toEqual(["blah"])
+  })
+  test("The query sanitizer can tokenize a string", () => {
+    const querySanitizer = new QuerySanitizer()
+
+    expect(querySanitizer.tokenize("this string")).toEqual(["this", "string"])
+  })
+  test("pass in string of multiple tokens incorrectly spelt and correct them", () => {
+    const querySanitizer = new QuerySanitizer({
+      yaers: "years",
+      yeild: "yield"
+    })
+
+    const incorrectQuery = "yaers yeild"
+    const correctlySpeltQuery = "years yield"
+    expect(querySanitizer.sanitize(incorrectQuery)).toEqual(correctlySpeltQuery)
+  })
 })
 
 //test our tokenizer
-//pass in string of multiple tokens incorrectly spelt and correct them
+//
 //pass in string containing a token that spell corrects to multiple tokens
+//pass in a non string
+// pass in an empty string
+// pass in a null
+// pass in a query string containing repeated whitespace
+// pass in a query string with punctuation chars
+// pass in a query string with only punctuation chars
--- site/src/utils/querysanitizer.js
+++ site/src/utils/querysanitizer.js
@@ -1,10 +1,28 @@
 import Dictionary from "./dictionary"
 export default class QuerySanitizer {
-  constructor() {
-    this.dictionary = new Dictionary({ yaers: "years" })
+  constructor(dictionaryDefinition) {
+    this.dictionary = new Dictionary(dictionaryDefinition)
   }
 
   sanitize(query) {
-    return this.dictionary.getCorrectSpelling(query)
+    const tokenizedQuery = this.tokenize(query)
+    let correctlySpeltTokens = []
+    tokenizedQuery.forEach(token => {
+      correctlySpeltTokens.push(this.dictionary.getCorrectSpelling(token))
+    })
+    const reassembledQuery = correctlySpeltTokens.join(" ")
+    return reassembledQuery
+  }
+
+  tokenize(query) {
+    // elasticlunr uses /[\s\-]+/
+    // return str.toString().trim().toLowerCase().split(elasticlunr.tokenizer.seperator);
+    let modifiedQuery = query
+    modifiedQuery
+      .toString()
+      .trim()
+      .toLowerCase()
+
+    return modifiedQuery.split(/[\s\-]+/)
   }
 }
