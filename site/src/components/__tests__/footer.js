f51b722528d239846559755429281e680650bba9
nphillipsworth <39960458+nphillipsworth@users.noreply.github.com>
2020-08-05 10:26:43 +0100

    ONS-714 extra analytical event (#297)
    * ONS-714 extra analytical event
    
    * removed unneeded file
    
    * change track event to make it more readable in matomo
    
    Co-authored-by: Nathan Phillips <nathanphillips@MacBook-Pro-7.broadband>

--- site/src/components/__tests__/footer.js
+++ site/src/components/__tests__/footer.js
@@ -1,7 +1,14 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render, fireEvent } from "react-testing-library"
 
 import Footer from "../footer"
+
+import { navigate } from "@reach/router"
+
+jest.mock('@reach/router', () => ({
+  navigate: jest.fn(),
+}))
 
 describe("Footer", () => {
   it("renders correctly", () => {
@@ -9,5 +16,24 @@ describe("Footer", () => {
       .create(<Footer/>)
       .toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it("test support page click has analytic event", () => {
+  	window._paq = []
+
+  	const { getByText } = render(<Footer phone_link={true}/>);
+  	// Census Field Support
+  	fireEvent.click(getByText('Census Field Support'));
+
+  	expect(navigate).toHaveBeenCalledTimes(1)
+    expect(navigate).toHaveBeenCalledWith('/contactcentre/')
+
+  	expect(window._paq[0]).toEqual([
+      "trackEvent",
+      "census-field-support",
+      "clicked help",
+      "/",
+      1
+    ])
   })
 })
